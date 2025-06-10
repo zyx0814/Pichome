43b78941096e6a3cefdbb456a81db25e2a387028
zyx0814 <zyx0814@163.com>
2025-06-10 18:15:38 +0800

    修复查看非授权文件的bug

--- dzz/textviewer/index.php
+++ dzz/textviewer/index.php
@@ -11,30 +11,28 @@ if (!defined('IN_OAOOA')) {
 }
 
 
-if(!isset($_GET['src']) && !$patharr=Pdecode($_GET['path'])){
+if(!$patharr=Pdecode($_GET['path'])){
     exit('Access Denied');
 }
-if($_GET['src']){
-	$str = file_get_contents(urldecode($_GET['src']));
-}else{
-    $rid = $patharr['path'];
-    $isshare = $patharr['isshare'];
-    $perm = $patharr['perm'];
-    $isadmin = $patharr['isadmin'];
-    global $_G;
-    if(strpos($rid, 'attach::') === 0){
-        $resourcesdata = C::t('attachment')->fetch(intval(str_replace('attach::', '', $rid)));
 
-    }else{
-        $resourcesdata = C::t('pichome_resources')->fetch_data_by_rid($rid);
-    }
-    if(!$resourcesdata['iswebsitefile'] && $resourcesdata['bz'] == 'dzz::'){
-        $fileurl  = getglobal('siteurl') . 'index.php?mod=io&op=getStream&path=' . dzzencode($rid.'_3', '', 14400, 0);
-    }else{
-        $fileurl=IO::getFileuri($resourcesdata['path']);
-    }
-    $str = file_get_contents($fileurl);
+$rid = $patharr['path'];
+$isshare = $patharr['isshare'];
+$perm = $patharr['perm'];
+$isadmin = $patharr['isadmin'];
+global $_G;
+if(strpos($rid, 'attach::') === 0){
+    $resourcesdata = C::t('attachment')->fetch(intval(str_replace('attach::', '', $rid)));
+
+}else{
+    $resourcesdata = C::t('pichome_resources')->fetch_data_by_rid($rid);
 }
+if(!$resourcesdata['iswebsitefile'] && $resourcesdata['bz'] == 'dzz::'){
+    $fileurl  = getglobal('siteurl') . 'index.php?mod=io&op=getStream&path=' . dzzencode($rid.'_3', '', 14400, 0);
+}else{
+    $fileurl=IO::getFileuri($resourcesdata['path']);
+}
+$str = file_get_contents($fileurl);
+
 
 require_once DZZ_ROOT . './dzz/class/class_encode.php';
 $p = new Encode_Core();
